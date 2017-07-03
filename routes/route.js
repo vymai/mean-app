46c9ef74816b567da41f8153ef3717fd503f7b19
vymai <vymai@users.noreply.github.com>
2017-07-03 15:48:55 -0700

    Contact logic (route.js)

--- routes/route.js
+++ routes/route.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
+// Retriving Contacts
 router.get('/contacts', (req, res, next) => {
     res.send('Retrieving the contact list');
+});
+
+// Add Contact
+router.post('/contact', (req, res, next) => {
+    // Logic to Add Contact
+});
+
+// Delete Contact
+router.delete('/contact/:id', (req, res, next) => {
+    // Logic to Delete Contact
 });
 
 module.exports = router;
