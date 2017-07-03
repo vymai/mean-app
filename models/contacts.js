47339c5e8685062e7b48b7cd4c2bc49dce3c40c2
vymai <vymai@users.noreply.github.com>
2017-07-03 15:51:15 -0700

    schema (contacts.js)

---
+++ models/contacts.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const ContactSchema = mongoose.Schema({
+    first_name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true
+    },
+    phone_number: {
+        type: String,
+        required: true
+    }
+})
