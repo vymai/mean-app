35dcb56cf06379c92ab7ed57346d2d0c75f4e703
vymai <vymai@users.noreply.github.com>
2017-07-04 14:04:33 -0700

    server=side done

--- routes/route.js
+++ routes/route.js
@@ -1,19 +1,41 @@
 const express = require('express');
 const router = express.Router();
 
+const Contact = require('../models/contacts');
+
 // Retriving Contacts
 router.get('/contacts', (req, res, next) => {
-    res.send('Retrieving the contact list');
+    Contact.find(function(err, contacts) {
+        res.json(contacts);
+    });
 });
 
 // Add Contact
 router.post('/contact', (req, res, next) => {
-    // Logic to Add Contact
+    let newContact = new Contact ({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        phone_number: req.body.phone_number
+    });
+
+    newContact.save((err, contact) => {
+        if (err) {
+            res.json({msg: 'Failed to add contact'});
+        } else {
+            res.json({msg: 'Contact added successfully'});
+        }
+    })
 });
 
 // Delete Contact
 router.delete('/contact/:id', (req, res, next) => {
-    // Logic to Delete Contact
+    Contact.remove({_id: req.params.id}, function(err, result) {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(result);
+        }
+    })
 });
 
 module.exports = router;
