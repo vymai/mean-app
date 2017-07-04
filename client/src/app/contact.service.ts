c34f63ba9cd3cbcd6fd89c8d7182c4d79fc89d6b
vymai <vymai@users.noreply.github.com>
2017-07-04 15:16:16 -0700

    done

---
+++ client/src/app/contact.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { Contact } from './contact';
+import 'rxjs/add/operator/map';
+ 
+@Injectable()
+export class ContactService {
+
+  constructor(private http: Http) { }
+
+  // Retrieving ContactService
+  getContacts() {
+    return this.http.get('http://localhost:3000/api/contacts')
+      .map(res => res.json());
+  }
+  
+  // Add Contact Method
+  addContact(newContact) {
+    var headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post('http://localhost:3000/api/contact', newContact, {headers: headers})
+      .map(res => res.json());
+  }
+
+  //Delete 
+  deleteContact(id) {
+    return this.http.delete('http://localhost:3000/api/contact/' + id)
+      .map(res => res.json());
+  }
+}
